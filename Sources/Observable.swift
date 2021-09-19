93f4761390ef553be046c69cc0f4df064a495b58
Valerio <>
2021-09-19 21:29:41 +0200

    assign function added

--- Sources/Observable.swift
+++ Sources/Observable.swift
@@ -1,8 +1,9 @@
 import Foundation
+import Combine
 
 @propertyWrapper
 public struct Observed<Value> {
-    private var observable: Observable
+    private var observable: Observable<Value>
     
     public var wrappedValue: Value {
         get {
@@ -21,49 +22,68 @@ public struct Observed<Value> {
         observable = .init(initialValue)
     }
     
-    /// Data Binding class without any 3rd party libraries
-    public final class Observable {
-        
-        struct Observer<Value> {
-            weak var observer: AnyObject?
-            let block: (_ oldValue: Value, _ newValue: Value) -> Void
-        }
-        
-        private var observers = [Observer<Value>]()
-        
-        fileprivate var value: Value {
-            didSet { notifyObservers(oldValue: oldValue) }
-        }
-        
-        public init(_ value: Value) {
-            self.value = value
-        }
-        
-        public func observe(on observer: AnyObject, observerBlock: @escaping (Value, Value) -> Void) {
-            // remove invalid observer before adding new ones
-            observers = observers.filter { $0.observer != nil }
-            observers.append(Observer(observer: observer, block: observerBlock))
-            observerBlock(value, value)
-        }
-        
-        public func remove(observer: AnyObject) {
-            observers = observers.filter { $0.observer !== observer }
-        }
-        
-        public func removeAllObservers() {
-            observers.removeAll()
-        }
-        
-        private func notifyObservers(oldValue: Value) {
-            for observer in observers {
-                DispatchQueue.main.async { observer.block(oldValue, self.value) }
-            }
-        }
-        
-    }
-    
     /// The property for which this instance exposes an Observable.
     ///
     /// The ``Observed/projectedValue`` is the property accessed with the `$` operator.
-    public var projectedValue: Observed<Value>.Observable { observable }
+    public var projectedValue: Observable<Value> { observable }
+}
+
+/// Data Binding class without any 3rd party libraries
+public final class Observable<Value> {
+    
+    struct Observer<Value> {
+        weak var observer: AnyObject?
+        let block: (_ oldValue: Value, _ newValue: Value) -> Void
+    }
+    
+    private var observers = [Observer<Value>]()
+    
+    private var bindedElements = NSMapTable<AnyObject, AnyObject>(keyOptions: .weakMemory, valueOptions: .weakMemory)
+    
+    fileprivate var value: Value {
+        didSet { notifyObservers(oldValue: oldValue) }
+    }
+    
+    public init(_ value: Value) {
+        self.value = value
+    }
+    
+    public func observe(on observer: AnyObject, observerBlock: @escaping (Value, Value) -> Void) {
+        // remove invalid observer before adding new ones
+        observers = observers.filter { $0.observer != nil }
+        observers.append(Observer(observer: observer, block: observerBlock))
+        observerBlock(value, value)
+    }
+    
+    public func remove(observer: AnyObject) {
+        observers = observers.filter { $0.observer !== observer }
+    }
+    
+    public func removeAllObservers() {
+        observers.removeAll()
+    }
+    
+    public func assign<R: AnyObject>(to keypath: ReferenceWritableKeyPath<R, Value>, on root: R) {
+        bindedElements.setObject(keypath, forKey: root)
+        root[keyPath: keypath] = self.value
+    }
+    
+    private func notifyObservers(oldValue: Value) {
+        for observer in observers {
+            DispatchQueue.main.async { observer.block(oldValue, self.value) }
+        }
+        
+        for key in bindedElements.keyEnumerator() {
+            let root = key as AnyObject
+            guard
+                let keyPath = bindedElements.object(forKey: root) as? ReferenceWritableKeyPath<AnyObject, Value>
+            else { continue }
+            updateKeyPath(keyPath, for: root)
+        }
+    }
+    
+    private func updateKeyPath<R: AnyObject>(_ keypath: ReferenceWritableKeyPath<R, Value>, for root: R) {
+        DispatchQueue.main.async { root[keyPath: keypath] = self.value }
+    }
+    
 }
